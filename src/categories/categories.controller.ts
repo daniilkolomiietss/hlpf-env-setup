e5814cf33896263ea606408522bf77c1bcaf94ad
daniilkolomiiets <daniilkolomii@gmail.com>
2026-04-28 12:40:52 +0300

    add JwtAuthGuard, RolesGuard, protect endpoints

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -7,42 +7,54 @@ import {
   Param,
   Body,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { Roles } from '../common/decorators/roles.decorator';
+import { Role } from '../common/enums/role.enum';
  
 @Controller('api/categories')
 export class CategoriesController {
   constructor(
-	private readonly categoriesService: CategoriesService,
+    private readonly categoriesService: CategoriesService,
   ) {}
  
   @Get()
   findAll() {
-	return this.categoriesService.findAll();
+    return this.categoriesService.findAll();
   }
  
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-	return this.categoriesService.findOne(id);
+    return this.categoriesService.findOne(id);
   }
  
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
   create(@Body() dto: CreateCategoryDto) {
-	return this.categoriesService.create(dto);
+    return this.categoriesService.create(dto);
   }
  
   @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
   update(
-	@Param('id', ParseIntPipe) id: number,
-	@Body() dto: UpdateCategoryDto,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateCategoryDto,
   ) {
-	return this.categoriesService.update(id, dto);
+    return this.categoriesService.update(id, dto);
   }
  
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
   remove(@Param('id', ParseIntPipe) id: number) {
-	return this.categoriesService.remove(id);
+    return this.categoriesService.remove(id);
   }
 }
